233f83b30df99b055b2142fa020c75d26ad42f1a
Hidehiro Fujio <fujio@jamstec.go.jp>
2026-01-20 07:14:53 +0900

    Element related headerfile restored.

---
+++ src/class/Interpolation.hpp
@@ -0,0 +1,85 @@
+/*
+ *  feelfem version 1.0  Copyright(c)  NEC Corporation 1999-2002
+ *                       Programmed by Hidehiro  FUJIO
+ *
+ *  Filename : Interpolation.hpp
+ *  Date     : 2002/03/03
+ *  Modified : 
+ *  
+ *  Purpose  :
+ *
+ * 
+ *  feelfem2 (modernized/ported)
+ *  Copyright (C) 2025-2026 Hidehiro Fujio and contributors
+ *  SPDX-License-Identifier: BSD-3-Clause
+ *  Repository: https://github.com/oimokoimo/feelfem2
+ *
+ *
+ *  Notes:
+ *  
+ */
+
+#ifndef FEM_CLASS_INTERPOLATION
+#define FEM_CLASS_INTERPOLATION
+
+#include "Node.hpp"
+#include "string.hpp"
+
+class ostream;
+
+class Interpolation {
+
+public:
+  Interpolation();
+  ~Interpolation();
+
+  void SetType(int); // INTERP_TYPE_NODAL,
+                     // INTERP_TYPE_DX
+                     // INTERP_TYPE_DY
+                     // INTERP_TYPE_DZ
+                     // INTERP_TYPE_DN
+                     // INTERP_TYPE_DTX  ...
+  int  GetType(void) {
+    return (type); 
+  }
+
+  void SetExprStr(char *);
+  void SetNodePtr( Node *);
+  void SetBoundaryNodePtr( Node *);
+
+  Node *GetNodePtr(void);        
+  Node *GetBoundaryNodePtr(void);  // for boundary
+  
+  string &GetExprString() { return exprStr; }
+
+  // following function checks type and Node *
+  friend int operator < (const Interpolation &left,const Interpolation &right);
+  friend int operator ==(const Interpolation &left,const Interpolation &right);
+
+
+  // generate boundary element for neumann type conditions
+  // return null if corresponding interpolation has no relationship in
+  // boundary reference face
+  Interpolation *GetBoundaryInterp_tri(void);   
+  Interpolation *GetBoundaryInterp_rect(void);
+  Interpolation *GetBoundaryInterp_tetra(void);
+  Interpolation *GetBoundaryInterp_cube(void);
+
+  void print(ostream &ost);
+
+private:
+  int   type;        // INTERP_TYPE_NODAL,
+                     // INTERP_TYPE_DX
+                     // INTERP_TYPE_DY
+                     // INTERP_TYPE_DZ
+                     // INTERP_TYPE_DN
+                     // INTERP_TYPE_DTX  ...
+  Node   *nodePtr;
+  Node   *boundaryNodePtr;
+  string  exprStr;
+
+};
+
+
+#endif
+
---
+++ src/class/Node.hpp
@@ -0,0 +1,63 @@
+/*
+ *  feelfem version 1.0  Copyright(c)  NEC Corporation 1999-2001
+ *                       Programmed by Hidehiro  FUJIO
+ *
+ *  Filename : Node.hpp       (instance is FiniteElement/Node.cpp)
+ *  Date     : 2002/01/03
+ *  Modified : 
+ *  
+ *  Purpose  : Node definition
+ *  
+ *
+ *  feelfem2 (modernized/ported)
+ *  Copyright (C) 2025-2026 Hidehiro Fujio and contributors
+ *  SPDX-License-Identifier: BSD-3-Clause
+ *  Repository: https://github.com/oimokoimo/feelfem2
+ *
+ *
+ *  Notes:
+ *
+ */
+
+#ifndef FEM_CLASS_NODE
+#define FEM_CLASS_NODE
+
+#include "orderedPtrList.hpp"
+class Element;
+class ostream;
+
+class Node {
+
+public:
+  Node(int);                        // 0-dimensional
+  Node(int,double);                 // 1-dimensional
+  Node(int,double,double);          // 2-dimensional
+  Node(int,double,double,double);   // 3-dimensional
+  ~Node();
+
+  //  AddNodeLstWithElement(orderedPtrList<Node *> &, Element * );  // SolveElement
+
+  double eval() const;
+
+  double GetXi(void)   { return xi;   }
+  double GetEta(void)  { return eta;  }
+  double GetZeta(void) { return zeta; }
+
+  friend int operator == (const Node &left, const Node &right);
+  friend int operator  < (const Node &left, const Node &right);
+
+  // debug
+  void print(ostream &ost);
+  
+private:
+  int dimension;
+  //  int degreeOfFreedom;
+  int etype;                 // element type (tri,rect,tetra,cube, etc.)
+  //  int ntype;                 // node type    (nodal, etc.)
+  //  int elementNodeNumber;
+
+  double xi,eta,zeta;
+
+};
+
+#endif
