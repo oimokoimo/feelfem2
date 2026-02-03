b227143ba1146d57ae40167f90a051a96f455981
Hidehiro Fujio <fujio@jamstec.go.jp>
2026-02-03 14:38:47 +0900

    Build command for WSL/ubuntu

---
+++ work/feelp2-wsl/src.inria98web/build.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+mkdir -p ~/bin
+mkdir -p ~/lib
+
+cd exec
+make
+cp ./feel ~/bin
+cd ..
+cp bamg/bamg ~/bin
+./clean.bat
+cd test
+make 
+cp parse ~/bin/feel.parse
+cd ../
+cd mesh_bamg_util/BAMG_BTOF_COMMAND
+make
+cp ./feel.bamg.exe ~/bin
+cd ../..
+cd feelG
+make
+cp libfeelG.a ~/lib
+
--- work/feelp2-wsl/src.inria98web/feel_def/feel_def.h
+++ work/feelp2-wsl/src.inria98web/feel_def/feel_def.h
@@ -631,7 +631,8 @@
 /* #define FreeBSD_CONTOUR_LIB        "-L/home/fujio/feel/lib.intel -lfeelG" */
 #define FreeBSD_CONTOUR_LIB        "-L/usr/people/sun1/fujio/lib -lfeelG"
 #define Cenju3_CONTOUR_LIB         "-lpfeelG"
-#define Linux_CONTOUR_LIB          "-L/home/fujio/lib -lfeelG"
+/* #define Linux_CONTOUR_LIB          "-L/home/fujio/lib -lfeelG" */
+#define Linux_CONTOUR_LIB          "-L~/lib -lfeelG"
 
 #define R4000_LIB                  "-Kmips2"
 
