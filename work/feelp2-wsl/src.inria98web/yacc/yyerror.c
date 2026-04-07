3f08051ccf38f954676bc96ccb315814bf46c446
Hidehiro Fujio <fujio@jamstec.go.jp>
2026-04-07 15:06:16 +0900

    line number is shown when parsing error in FEEL p2

--- work/feelp2-wsl/src.inria98web/yacc/yyerror.c
+++ work/feelp2-wsl/src.inria98web/yacc/yyerror.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
+extern int yylineno;
+
 int yyerror(const char *s)
 {
-    fprintf(stderr, "FEEL parse error: %s\n", s);
+    fprintf(stderr, "FEEL parse error:(%d)  %s\n",yylineno, s);
     return 0;
 }
 
